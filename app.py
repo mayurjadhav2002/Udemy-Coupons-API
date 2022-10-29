c7d26afb1159b04d0071e186a28e81a68da7b0d7
mayurjadhav2002 <mayurshrikantjadhav@gmail.com>
2022-10-29 19:29:06 +0530

    new commit

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ if __name__ == '__main__':
         pass
     finally:
            db.create_all()
-           app.run(debug=True, host='0.0.0.0', port='5000')
+           app.run(host='0.0.0.0', port='5000')
